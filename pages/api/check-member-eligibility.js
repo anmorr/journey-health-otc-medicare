0c324bf525360128970c99083ac3febae1974853
Anthony Morris <myThinkTank@Anthonys-MacBook-Pro.local>
2022-09-16 20:21:40 -0700

    Added api to call the backend api

---
+++ pages/api/check-member-eligibility.js
@@ -0,0 +1,32 @@
+
+import axios from 'axios';
+
+export default async function handler(req, res) {
+
+    if (req.method === 'POST') {
+        const data = req.body
+        // console.log("data: ", data)
+        try {
+            const response = await axios({
+                method: 'post',
+                'url' : 'https://mm23tf5iwh.execute-api.us-west-1.amazonaws.com/Prod/eligibility',
+                headers: {
+                    // Authorization: authorization,
+                    "Content-Type": "application/json"
+                },
+                data: data
+            });
+            // console.log("response=> ", res.statusCode)
+            // console.log("response=> ", res.json())
+            // res.status = res.status
+            const responseData = await response.data
+            // console.log("responseData: ", responseData)
+            res.status(res.statusCode).json({...responseData})
+        } catch (error) {
+            console.log("res: ", res)
+            res.json()
+            
+        }
+    }
+    
+  }
